858d8de9ba2fd46f50dceae321df56a5a6b92237
Raimon230 <131254450+Raimon230@users.noreply.github.com>
2026-05-23 12:29:39 +0200

    Add exercise routines for push, pull, legs, and upper/lower

--- src/data/routines.js
+++ src/data/routines.js
@@ -1,1 +1,48 @@
+export const routines = {
+  push: [
+    "Press inclinado multipower",
+    "Press hombro multipower",
+    "Tríceps polea",
+    "Tríceps francés polea",
+    "Flexiones"
+  ],
 
+  pull: [
+    "Flexiones",
+    "Remo en T",
+    "Jalón al pecho",
+    "Remo polea",
+    "Pull over",
+    "Bíceps polea",
+    "Face pull"
+  ],
+
+  legs: [
+    "Aductor máquina",
+    "Cuádriceps máquina",
+    "Isquios máquina",
+    "Prensa",
+    "Gemelo prensa"
+  ],
+
+  upper: [
+    "Flexiones",
+    "Press multipower",
+    "Press hombro multipower",
+    "Jalón pecho",
+    "Remo polea",
+    "Tríceps polea",
+    "Tríceps francés",
+    "Bíceps polea",
+    "Face pull"
+  ],
+
+  lower: [
+    "Hip thrust",
+    "Aductor",
+    "Cuádriceps polea",
+    "Isquios polea",
+    "Prensa",
+    "Gemelos prensa"
+  ]
+}
